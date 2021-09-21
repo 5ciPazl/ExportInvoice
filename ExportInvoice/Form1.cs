dfb0dc63c873690c90d98eba160693ccea6b3518
5ciPazl <82058820+5ciPazl@users.noreply.github.com>
2021-09-21 12:11:57 +0400

    Add project files.

---
+++ ExportInvoice/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExportInvoice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnectionStringBuilder sql = new SqlConnectionStringBuilder();
+            sql.DataSource = textBox1.Text;
+            sql.UserID = textBox2.Text;
+            sql.Password = textBox3.Text;
+            sql.InitialCatalog = "CashExpert";
+            SqlConnection connection = new SqlConnection(sql.ConnectionString);
+            try
+            {
+                connection.Open();
+                Form2.conStr = sql.ConnectionString;
+                Form2 form2 = new Form2();
+                form2.ShowDialog();
+            }
+            catch
+            {
+                MessageBox.Show("Qoşulma uğursuzdur.");
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form2.cs
@@ -0,0 +1,90 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public static string conStr;
+        DataTable td;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Invoice id yanlışdır.");
+                return;
+            }
+            else
+            {
+                try
+                {
+                    int a = int.Parse(textBox1.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Invoice id yanlışdır.");
+                    return;
+                }
+            }
+
+            int id = int.Parse(textBox1.Text);
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(conStr))
+                {
+                    con.Open();
+
+                    using (SqlCommand command = new SqlCommand())
+                    {
+                        command.Connection = con;
+                        command.CommandText = "SELECT * FROM [CashExpert].[dbo].[InvoiceDetails] where InvoiceID = @id";
+                        command.Parameters.AddWithValue("id", id);
+
+                        using(SqlDataAdapter adapter = new SqlDataAdapter(command))
+                        {
+                            command.ExecuteNonQuery();
+
+                            DataTable dt = new DataTable();
+                            adapter.Fill(dt);
+                            td = dt;
+                            dataGridView1.DataSource = dt;
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Title = "Invoice_" + textBox1.Text + ".xlsx";
+            saveFile.ShowDialog();
+
+
+            XLWorkbook wb = new XLWorkbook();
+            td.Columns.Remove(td.Columns[13]);
+            wb.Worksheets.Add(td, "WorksheetName").Column(14).Width = 100;
+            wb.SaveAs(saveFile.FileName);
+            MessageBox.Show("Uğurlu.");
+        }
+    }
+}
